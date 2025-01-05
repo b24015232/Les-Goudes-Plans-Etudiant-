405d670508533a029e216b8867696e6d000aac59
b24015232 <heather.BURBECK@etu.univ-amu.fr>
2025-01-05 14:29:23 +0100

    test version mobile

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ document.addEventListener("DOMContentLoaded", function() {
 /*Menu version mobile*/
 
 function myFunction() {
-    var x = document.getElementById("myT");
+    var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
-        x.className = "topnav-mobile";
+        x.className = "topnav";
     }
 }
 
